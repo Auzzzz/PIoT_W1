56531bc4cefcef35ca5cfb357b85b0edea4bb796
Auzzzz <s3544232@student.rmit.edu.au>
2020-03-26 14:19:09 +1100

    debug hashed & colours added

--- test.py
+++ test.py
@@ -1,2 +1,21 @@
-print("Hello World")
-print("Bye World")
+#print("Hello World")
+#print("Bye World")
+
+temp = 10
+
+def displaytemp():
+    t = int(round(temp))
+    if t < 10:
+        print("its cold")
+    elif t in range(10,23):
+        print("Compfhy ")
+    elif t >= 23:
+        print("HOT")
+    else:
+        print("error")
+    
+    
+    
+displaytemp()
+
+
